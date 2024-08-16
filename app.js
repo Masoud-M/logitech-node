c08f424195e70747341c75f94d264b99e0cd914c
Masoud-M <masoudmoghaddam999@gmail.com>
2024-08-16 23:41:22 +0330

    Initial commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const Product = require("./models/product");
+
+// express app
+const app = express();
+
+// connect to mongodb & listen for requests
+const dbURI =
+  "mongodb+srv://notadmin:37UiVr0Qh59l0lA5@cluster0.1s00v.mongodb.net/logitech?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose
+  .connect(dbURI)
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// register view engine
+app.set("view engine", "ejs");
+
+// middleware & static files
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
+
+// routes
+
+// product routes
+app.get("/", (req, res) => {
+  Product.find()
+    .then((result) => {
+      res.render("index", { products: result, title: "All Products" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/:id", (req, res) => {
+  const id = req.params.id;
+
+  Product.findById(id)
+    .then((result) => {
+      res.render("details", { products: result, title: "Product Details" });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render("404", { title: "Product not found" });
+    });
+});
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404" });
+});
---
+++ models/product.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  _id: Schema.Types.ObjectId,
+  name: String,
+  series: String,
+  colors: [String],
+  price: Number,
+  on_sale: Boolean,
+  discount: Number,
+  thumbnail_url: String,
+  in_stuck: Boolean,
+  best_seller: Boolean,
+  rating: Number,
+  reviews: Number,
+  description: String,
+  category: String,
+  gallery: Schema.Types.Mixed,
+  misc: Schema.Types.Mixed,
+});
+
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
